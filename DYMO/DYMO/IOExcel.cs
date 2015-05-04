179704dc40f4455aa08ff9a9f0af63201588a8db
Kevin Reynolds <kevinmr54@yahoo.com>
2015-05-03 17:10:10 -0700

    Excel Import & Timesheet 2
    I added my DYMO folder which contains a very basic set-up for importing
    an excel spreadsheet. Currently, it's formatted to work with the fake
    data spreadsheet which I included in the debug area in the folder.
    
    IOExcel is the class that does the busy work, but Main needs to have
    [STAThread] to function correctly.

---
+++ DYMO/DYMO/IOExcel.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Data.OleDb;
+using System.Windows.Forms;
+
+namespace DYMO
+{
+    class IOExcel
+    {
+        
+        public IOExcel()
+        {
+        }
+
+        public string selectFile()
+        {
+            OpenFileDialog openFileDialog1 = new OpenFileDialog();
+
+            // Setting the filter options
+            openFileDialog1.Filter = "Microsoft Excel Worksheet (.xlsx)|*.xlsx|All Files (*.*)|*.*";
+            openFileDialog1.FilterIndex = 1;
+            openFileDialog1.Multiselect = false;
+
+            // Check if user selected file or not
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+                return openFileDialog1.FileName;
+            else
+                return null;
+        }
+
+        public void importExcel(string fileName)
+        {
+            string con = @"Provider=Microsoft.ACE.OLEDB.12.0;Data Source=" + fileName + ";Extended Properties='Excel 12.0 xml;HDR=Yes;'";
+            using (OleDbConnection connection = new OleDbConnection(con))
+            {
+                connection.Open();
+                OleDbCommand command = new OleDbCommand("select * from [Sheet1$]", connection);
+                using (OleDbDataReader dr = command.ExecuteReader())
+                {
+                    dr.Read();
+
+
+                    while (dr.Read())
+                    {
+                        //var row1Col0 = dr[0];
+                        Console.WriteLine(dr[0] + " " + dr[1] + " " + dr[2] + " " + dr[3] + " " + dr[4]);
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ DYMO/DYMO/Printer.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web;
+
+namespace DYMO
+{
+    public class Printer
+    {
+        [STAThread]
+        static void Main(string[] args)
+        {
+            IOExcel ioe = new IOExcel();
+
+            Console.WriteLine("Select .xlsx file.");
+            string filename = ioe.selectFile();
+            ioe.importExcel(filename);
+
+            Console.WriteLine("Enter anything to quit.");
+            Console.ReadLine();
+            
+        }
+
+        public void Print(string firstName, string lastName)
+        {
+            // Prints
+            /*var label = DYMO.Label.Framework.Label.Open("C:/Users/Kevin/Documents/Visual Studio 2013/WebSites/DYMO/LargeAddressTestLabel.label");
+            label.SetObjectText("Address", "DYMOn828 San Pablo AvenAlbany CA 94706");
+            label.Print("DYMO LabelWriter 450 DUO Label");*/
+        }
+
+    }
+}
